704278794d8127d17e943fad34b51011eea148aa
Mick Jasinski <mickjasinski@gmail.com>
2018-05-03 17:51:42 +0100

    Generate about and navogation modules

---
+++ client/modules/Navigation/NavigationReducer.js
@@ -0,0 +1,14 @@
+// Import Actions
+import {  } from './NavigationActions';
+
+// Initial State
+const initialState = {};
+
+const NavigationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default NavigationReducer;
